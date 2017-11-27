2848335f8947a6fa63ce9e98dcdb468c68c484fa
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2017-11-26 23:47:19 -0600

    Add new model attributes

--- app/models/sat/catalogo/clave_prod_serv.rb
+++ app/models/sat/catalogo/clave_prod_serv.rb
@@ -2,7 +2,10 @@ module Sat
   module Catalogo
     class ClaveProdServ < Base
       self.table_name = 'sat_catalogo_clave_prod_serv'
-      attr_accessible :sat_id, :descripcion, :fecha_inicio_vigencia, :fecha_fin_vigencia, :incluir_iva_trasladado, :incluir_ieps_trasladado, :complemento_que_debe_incluir
+      attr_accessible :sat_id, :descripcion, :fecha_inicio_vigencia,
+                      :fecha_fin_vigencia, :incluir_iva_trasladado,
+                      :incluir_ieps_trasladado, :complemento_que_debe_incluir,
+                      :palabras_similares
      end
   end
 end
