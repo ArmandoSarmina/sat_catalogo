4db70e9b880ffb60f4c9739e48c68e128b268882
Ernesto Alcaraz Andrade <ernesto.alcaraz@magmalabs.io>
2017-11-30 13:08:20 -0600

    overwrite searchable columns

--- app/models/sat/catalogo/clave_unidad.rb
+++ app/models/sat/catalogo/clave_unidad.rb
@@ -4,6 +4,10 @@ module Sat
       self.table_name = 'sat_catalogo_clave_unidad'
       attr_accessible :sat_id, :nombre, :descripcion, :nota, :fecha_de_inicio_de_vigencia, :fecha_de_fin_de_vigencia, :simbolo
 
+      def searchable_columns
+        [:nombre, :sat_id]
+      end
+
       def as_json
         { label: nombre, value: sat_id }
       end
