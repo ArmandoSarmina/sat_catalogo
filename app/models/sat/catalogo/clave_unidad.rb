dd9d78f24bd8ba337e63e3e181eb686f2f7c3349
Ernesto Alcaraz Andrade <ernesto.alcaraz@magmalabs.io>
2017-12-01 18:30:43 -0600

    when overwriting the searchable_columns method need to be a class method

--- app/models/sat/catalogo/clave_unidad.rb
+++ app/models/sat/catalogo/clave_unidad.rb
@@ -4,7 +4,7 @@ module Sat
       self.table_name = 'sat_catalogo_clave_unidad'
       attr_accessible :sat_id, :nombre, :descripcion, :nota, :fecha_de_inicio_de_vigencia, :fecha_de_fin_de_vigencia, :simbolo
 
-      def searchable_columns
+      def self.searchable_columns
         [:nombre, :sat_id]
       end
 
